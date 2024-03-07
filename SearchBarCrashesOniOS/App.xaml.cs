f18d139bd5568c652a1d396c3d2d32d8c868561a
Justin Cook <justincook2003.work@gmail.com>
2024-03-07 11:47:30 -0600

    Add project files.

---
+++ SearchBarCrashesOniOS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SearchBarCrashesOniOS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SearchBarCrashesOniOS/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SearchBarCrashesOniOS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
